f7acbf5f644b736f5d67e99171790a138bc5ecf2
RosieOh <20172207@gm.hannam.ac.kr>
2024-05-26 19:57:10 +0900

    ✨BUILD : Vue.js 3버전 기본 환경 세팅

---
+++ src/components/common/HeaderView.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app-bar color="headerBg">
+        <v-btn @click="movePage('/politics')">Ecomo Automat</v-btn>
+        <v-spacer></v-spacer>
+        <v-card-text>
+            <v-text-field
+                v-model="searchKeyword"
+                @keyup.enter="search"
+                density="compact"
+                variant="solo"
+                label="Search keyword"
+                append-inner-icon="mdi:magnify"
+                single-line
+                hide-details
+            ></v-text-field>
+        </v-card-text>
+        <v-spacer></v-spacer>
+        <v-btn icon="dark_mode" @click="toggleTheme" v-if="theme.current.value.dark"></v-btn>
+        <v-btn icon="light_mode" @click="toggleTheme" v-else></v-btn>
+        <div v-if="!profile">
+            <v-btn
+                icon="mdi"
+            ></v-btn>
+
+        </div>
+    </v-app-bar>
+    <div>
+        
+    </div>
+</template>
